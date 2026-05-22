da5532e7d7685932fbcc968cca66eec4ff5bdc2a
bbb <132804471+beambeambeam@users.noreply.github.com>
2026-05-22 18:35:23 +0700

    Add Docker DB env config and initial auth migration (#3)
    feat: add docker db env config and initial migration

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
 dotenv.config({
-  path: "../../apps/server/.env",
+  path: "./.env",
 });
 
 export default defineConfig({
