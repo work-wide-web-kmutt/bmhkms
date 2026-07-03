3da59fc4e800597a4a46c015318b5ceaf36a6265
beambeambeam <supawitmarayat@gmail.com>
2026-07-03 16:39:43 +0700

    chore: ignore migration files

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     "apps/server/dist/**",
     "packages/db/dist/**",
     "packages/db/src/schema/auth.ts",
+    "packages/db/src/migrations",
   ],
   options: {
     typeAware: false,
