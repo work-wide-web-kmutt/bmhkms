aa04427720ccb6671606e8d9564102df83c291b7
beambeambeam <supawitmarayat@gmail.com>
2026-07-03 14:30:01 +0700

    fix: vp check for css

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite-plus";
 export default defineConfig({
   staged: {
     "!(*.gen).ts": "ultracite fix",
-    "*.{js,jsx,tsx,vue,svelte,css,md}": "ultracite fix",
-    "*.{json,jsonc}": "vp check --fix",
+    "*.{css,md,json,jsonc}": "vp check --fix",
+    "*.{js,jsx,tsx,vue,svelte}": "ultracite fix",
   },
 });
