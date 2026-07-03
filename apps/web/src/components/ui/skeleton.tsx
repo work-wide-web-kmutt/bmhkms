3f03695c63063f4c6e7a1d5d89a36d5564bf8f09
beambeambeam <supawitmarayat@gmail.com>
2026-07-03 14:49:32 +0700

    feat: re-install shadcn ui components

--- apps/web/src/components/ui/skeleton.tsx
+++ apps/web/src/components/ui/skeleton.tsx
@@ -1,12 +1,10 @@
-import type * as React from "react";
-
 import { cn } from "@/lib/utils";
 
 function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("animate-pulse rounded-none bg-muted", className)}
+      className={cn("animate-pulse rounded-md bg-muted", className)}
       {...props}
     />
   );
