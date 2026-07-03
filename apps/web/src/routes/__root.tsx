46ad7d538c92e2c43671b2f5f7be1407b41f5666
beambeambeam <supawitmarayat@gmail.com>
2026-07-03 14:12:37 +0700

    feat: clean up bts boilerplate

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -7,7 +7,6 @@ import {
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-import Header from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import type { orpc } from "@/utils/orpc";
@@ -54,7 +53,6 @@ function RootComponent() {
         storageKey="vite-ui-theme"
       >
         <div className="grid grid-rows-[auto_1fr] h-svh">
-          <Header />
           <Outlet />
         </div>
         <Toaster richColors />
