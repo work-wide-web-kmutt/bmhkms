19556f48bafc28a20b7459c3354f2b55388c8474
beambeambeam <supawitmarayat@gmail.com>
2026-07-06 21:58:59 +0700

    feat: add text on header

--- apps/staff/src/components/staff-sidebar.tsx
+++ apps/staff/src/components/staff-sidebar.tsx
@@ -3,11 +3,13 @@ import {
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
+  SidebarHeader,
 } from "@/components/ui/sidebar";
 
 export function StaffSidebar() {
   return (
     <Sidebar collapsible="offcanvas" variant="inset">
+      <SidebarHeader>Bangmod Hackathon Management System</SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupContent />
